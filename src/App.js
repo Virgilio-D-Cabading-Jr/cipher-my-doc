c9dc33be3cabad27cb344efc1abcc6ac5e318265
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-24 11:11:18 -0700

    create a useState for location

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
 import NavBar from './components/NavBarComp';
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
+import {React, useState} from 'react';
 
 ////////////////////////////////////////
 // App.js
 ////////////////////////////////////////
 
 function App() {
+    // //// VARIABLES //////////////////////////////
+    const [location, setLocation] = useState("NotHome");
+
+
+  // //// OUTPUT /////////////////////////////////
   return (
     <main className='bg'>
       {/* *** Nav Bar Component *** */}
       <NavBar />
-      <Switch>
         <div className="container mt-4">
           <div className="row bg-white p-3 round">
             <div className="col">
               {/* **** Home Route ******** */}
-              <Route exact path="/">
-                {/* <HomeComp /> */}
-                <h1>Hello World</h1>
-              </Route>
-              <Route exact path="/anotherpage">
-                {/* <HomeComp /> */}
-                <h1>This is another page</h1>
-              </Route>
-              {/* **** Root Redirect to Home ******** */}
-              {/* <Route path="/">
-                <div >
-                  <Redirect to="/home" />
-                </div>
-              </Route> */}
+              {/* <HomeComp /> */}
+              {
+                (location === "Home")
+                  ? <h1>Hello World I'm at Home</h1>
+                  : <h1>Sorry I'm not home</h1>
+              }
             </div>
           </div>
         </div>
-      </Switch>
     </main>
   );
 }
