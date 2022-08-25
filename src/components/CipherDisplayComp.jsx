270c5ad10bc68473e692b4300303cf30864d57b9
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-25 14:22:37 -0700

    apply ceasar cipher after form submitted in cipher display

--- src/components/CipherDisplayComp.jsx
+++ src/components/CipherDisplayComp.jsx
@@ -13,12 +13,57 @@ const CipherDisplayComp = (props) => {
 
     // //// UTILITY FUNCTIONS ///////////////////////
 
+    /**
+     * Ceasar Cipher
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER k
+     * The function is expected to return a STRING.
+     */
+
+    const caesarCipher = (s, k) => {
+        // Variables
+        const alphaMap = {
+            'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25
+        };
+        const alphaArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        let output = "";
+
+        // Iterate through the string
+        //      for each letter in the string
+        //          determine if it is a capital or lower case letter
+        //          shift the letter by #k
+        //          memorize the shifted letter to output string
+        for (let currentLet of s) {
+            // console.log({currentLet});
+            // Check if current letter is a letter
+            if (currentLet.toLowerCase() != currentLet.toUpperCase()) {
+                const isCapital = currentLet === currentLet.toUpperCase();
+                // console.log({isCapital});
+                const shiftAmt = (alphaMap[currentLet.toLowerCase()] + k) % 26;
+                // console.log({shiftAmt});
+                if (isCapital) {
+                    output += alphaArray[shiftAmt].toUpperCase();
+                } else {
+                    output += alphaArray[shiftAmt];
+                }
+            } else {
+                output += currentLet;
+            }
+        }
+        console.log({ output });
+        return output;
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
         setCipherSubmitted(true);
+        setCipheredText(caesarCipher(textToCipher, 3))
         console.log("**** In Cipher Display, Cipher submitted:");
         console.log({ textToCipher });
     };
+
+
 
     // //// OUTPUT //////////////////////////////////
     return (
@@ -44,21 +89,22 @@ const CipherDisplayComp = (props) => {
                             <button type="submit" className="btn btn-success round-btn mt-3">
                                 <strong>🔑 Apply Cipher to Text</strong>
                             </button>
-                            {
-                                cipherSubmitted
-                                    ?
-                                    <form>
-                                        <div className="form-group mb-2">
-                                            <label htmlFor="cipheredText">Ciphered Text:</label>
-                                            <textarea className="form-control"
-                                                id="cipheredText" rows="10"
-                                                value={cipheredText} />
-                                        </div>
-                                    </form>
-                                    : <div></div>
-                            }
                         </div>
                     </form>
+                    {
+                        cipherSubmitted
+                            ?
+                            <form>
+                                <div className="form-group mb-2">
+                                    <label htmlFor="cipheredText">Ciphered Text:</label>
+                                    <textarea className="form-control"
+                                        id="cipheredText" rows="10"
+                                        value={cipheredText}
+                                        onChange={e => setCipheredText(e.target.value)} />
+                                </div>
+                            </form>
+                            : <div></div>
+                    }
                 </div>
             </div >
         </div >
