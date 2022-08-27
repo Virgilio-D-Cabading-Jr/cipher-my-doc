baadd37ec3f515909265f354d78a56181e0089f7
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-27 09:47:42 -0700

    add de-cipher output comp to de-cipher display comp

--- src/components/DeCipherDisplayComp.jsx
+++ src/components/DeCipherDisplayComp.jsx
@@ -1,4 +1,5 @@
 import {React, useState} from 'react'
+import DeCipherOutputComp from './DeCipherOutputComp';
 
 /////////////////////////////////////////////////////
 // DeCipher Display Component
@@ -8,6 +9,7 @@ const DeCipherDisplayComp = (props) => {
     // //// VARIABLES ///////////////////////////////
     const location = props.location;
     const [decipherSubmitted, setDecipherSubmitted] = useState(false);
+    const [deCipherModifier, setDeCipherModifier] = useState(0);
     const [textToDeCipher, settextToDeCipher] = useState("");   // Text to be De-Ciphered
 
     // //// UTILITY FUNCTIONS ///////////////////////
@@ -16,7 +18,7 @@ const DeCipherDisplayComp = (props) => {
      * Handle Submit Function
      */
     const handleSubmit = (e) => {
-        e.prevenDefault();          // Prevents browser from refreshing when form submitted
+        e.preventDefault();          // Prevents browser from refreshing when form submitted
         setDecipherSubmitted(true);
     }
 
@@ -26,6 +28,7 @@ const DeCipherDisplayComp = (props) => {
             <div className="row bg-white p-3 round">
                 <div className="col">
                     <p>DeCipher Display Component</p>
+                    {/* **** Text Input Form ******** */}
                     <form onSubmit={e => handleSubmit(e)} >
                         <div className='row'>
                             <div className='col m-2'>
@@ -42,7 +45,29 @@ const DeCipherDisplayComp = (props) => {
                                 </div>
                             </div>
                         </div>
+                        {/* **** De-Cipher Controls ******** */}
+                        <div className='row bg-dark text-white m-2 p-3 round'>
+                            {/* **** De-Cipher Type ******** */}
+                            <h4 className='col m-2'>Ceasar Cipher</h4>
+                            {/* **** De-Cipher Modifier ******** */}
+                            <div className="col m-2">
+                                <div className='row'>
+                                    <label className='col' htmlFor="deCipherModifier">Amount to shift:</label>
+                                    <div className="col m-2">
+                                        <input type="number"
+                                            id="deCipherModifier"
+                                            value={ deCipherModifier }
+                                            onChange={e => setDeCipherModifier(parseInt(e.target.value))} />
+                                    </div>
+                                </div>
+                            </div>
+                            {/* **** Submit Button ******** */}
+                            <button type="submit" className="btn btn-success round-btn col">
+                                <strong>🔓 De-Cipher Text</strong>
+                            </button>
+                        </div>
                     </form>
+                    <DeCipherOutputComp />
                 </div>
             </div>
         </div>
---
+++ src/components/DeCipherOutputComp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DeCipherOutputComp = (props) => {
+  return (
+    <div>DeCipherOutputComp</div>
+  )
+}
+
+export default DeCipherOutputComp
